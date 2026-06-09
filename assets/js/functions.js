0c4b9c80a5c1df8e306156f8d159dd2e765d1600
Roger Tan <teochewthunder@gmail.com>
2026-06-09 23:40:54 +0800

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,4 @@
-function setupNameValidation(id) {
-    const re = /[^\u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF]/g;
-
-    const input = document.getElementById(id);
-
-    input.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(re, "");
-    });
+function isValidChineseName(name) {
+    const re = /^[\u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF]+$/;
+    return re.test(name);
 }
