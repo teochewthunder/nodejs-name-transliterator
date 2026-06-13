6de3d30cfb3f7f172bcaa88f349212f486620225
Roger Tan <teochewthunder@gmail.com>
2026-06-14 00:04:13 +0800

    Create options.js

---
+++ assets/data/options.js
@@ -0,0 +1,24 @@
+const options = [
+    {
+        value: "wade_giles",
+        label: "Mandarin (Wade-Giles)",
+        prompt: "Mandarin using Wade-Giles romanization"
+    },
+    {
+        value: "jyutping",
+        label: "Cantonese (Jyutping)",
+        prompt: "Cantonese using Jyutping with tone numbers"
+    },
+    {
+        value: "korean",
+        label: "Korean (Hangul)",
+        prompt: "Korean in Hangul"
+    },
+    {
+        value: "vietnamese",
+        label: "Vietnamese (Sino-Vietnamese)",
+        prompt: "Vietnamese Sino-Vietnamese reading"
+    }
+];
+
+module.exports = options;
