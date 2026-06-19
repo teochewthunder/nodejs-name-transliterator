07336d47bf470ffd875b968720ad37abdce870c0
Roger Tan <teochewthunder@gmail.com>
2026-06-19 23:43:22 +0800

    Formatting

--- assets/data/options.js
+++ assets/data/options.js
@@ -1,28 +1,23 @@
 const options = [
     {
-        value: "wade_giles",
+        value: "wade-giles",
         label: "Wade-Giles",
-        prompt: "Wade-Giles romanization"
     },
     {
         value: "hokkien",
         label: "Hokkien",
-        prompt: "Hokkien romanization"
     },
     {
-        value: "cantonese",
+        value: "jyutping",
         label: "Cantonese",
-        prompt: "Cantonese (Jyutping) romanization"
     },
     {
         value: "korean",
         label: "Korean",
-        prompt: "Korean (Hangul) romanization"
     },
     {
         value: "vietnamese",
         label: "Vietnamese",
-        prompt: "Vietnamese Sino-Vietnamese romanization"
     }
 ];
 
